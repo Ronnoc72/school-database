64b833d9b3d7dee018c485804239630520d6ed43
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-12-03 12:21:19 -0700

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS school_db;
+CREATE DATABASE school_db;
+USE school_db;
+
+CREATE TABLE courses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description VARCHAR(200) NOT NULL,
+  course_num INT NOT NULL
+);
+
+CREATE TABLE staff (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  age INT DEFAULT 0,
+  program VARCHAR(50) NOT NULL,
+  course_id INT NOT NULL,
+  FOREIGN KEY(course_id) REFERENCES courses(id)
+);
+
+CREATE TABLE students (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  age INT DEFAULT 0,
+  major VARCHAR(50) NOT NULL,
+  current_course VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE certificates (
+  major VARCHAR(50) NOT NULL,
+  enrolled INT DEFAULT 0,
+  courses VARCHAR(200) NOT NULL,
+  graduated INT DEFAULT 0
+);
