cc812ef5d79bfa16a45f8a7f3cc59859ca1ea257
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-12-03 12:36:08 -0700

    Create practice_data.sql

---
+++ practice_data.sql
@@ -0,0 +1,19 @@
+INSERT INTO courses (name, description, course_num) VALUES
+("computer science", "science about the computer", 1),
+("creative writing", "write cool stories", 2),
+("biology", "organisms", 3),
+("floraculure", "the science of aranging flowers", 4),
+("welding", "putting pieces of metal together", 5);
+
+INSERT INTO staff (name, age, program, course_id) VALUES
+("Green", 42, "floraculure", 4),
+("Felch", 35, "biology", 3),
+("Vince", 36, "computer science", 1),
+("Evans", 28, "welding", 5);
+
+INSERT INTO students (name, age, major, current_course) VALUES
+("Willams", 21, "computer science", 1),
+("Thaker", 20, "computer science", 1),
+("Peb", 19, "floraculure", 4),
+("Cedar", 22, "biology", 3),
+("Habit", 25, "computer science", 1);
